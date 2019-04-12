4a860aec714302ecf3f396d54085374c697c6158
Marco Miranda <marmirand@hotmail.com>
2019-04-12 12:22:34 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -31,5 +31,14 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  whole_season = data.find { | s_title, s_data |  s_title == season } [1]
+
+   sum = 0
+  count = 0
+  whole_season.each do |data|
+    sum += data["age"].to_i
+    count += 1
+  end
+
+  return (sum.to_f / count).round
 end
